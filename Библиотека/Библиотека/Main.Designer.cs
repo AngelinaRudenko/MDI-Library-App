1a869493e1dd3da9679dbe8ea3efb8da3db3a6f1
Angelina Rudenko <angel.rudenko.777@gmail.com>
2019-12-06 22:19:31 +0300

    MDI Windows Forms Library App

---
+++ Библиотека/Библиотека/Main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Library
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.импортэкспортToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.импортToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.экспортToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.окноToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.управлениеКнигамиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.управлениеПользователямиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.импортэкспортToolStripMenuItem,
+            this.окноToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // импортэкспортToolStripMenuItem
+            // 
+            this.импортэкспортToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.импортToolStripMenuItem,
+            this.экспортToolStripMenuItem});
+            this.импортэкспортToolStripMenuItem.Name = "импортэкспортToolStripMenuItem";
+            this.импортэкспортToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.импортэкспортToolStripMenuItem.Text = "Импорт/экспорт";
+            // 
+            // импортToolStripMenuItem
+            // 
+            this.импортToolStripMenuItem.Name = "импортToolStripMenuItem";
+            this.импортToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.импортToolStripMenuItem.Text = "Импорт";
+            // 
+            // экспортToolStripMenuItem
+            // 
+            this.экспортToolStripMenuItem.Name = "экспортToolStripMenuItem";
+            this.экспортToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.экспортToolStripMenuItem.Text = "Экспорт";
+            // 
+            // окноToolStripMenuItem
+            // 
+            this.окноToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.управлениеКнигамиToolStripMenuItem,
+            this.управлениеПользователямиToolStripMenuItem});
+            this.окноToolStripMenuItem.Name = "окноToolStripMenuItem";
+            this.окноToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.окноToolStripMenuItem.Text = "Окно";
+            // 
+            // управлениеКнигамиToolStripMenuItem
+            // 
+            this.управлениеКнигамиToolStripMenuItem.Name = "управлениеКнигамиToolStripMenuItem";
+            this.управлениеКнигамиToolStripMenuItem.Size = new System.Drawing.Size(234, 22);
+            this.управлениеКнигамиToolStripMenuItem.Text = "Управление книгами";
+            this.управлениеКнигамиToolStripMenuItem.Click += new System.EventHandler(this.УправлениеКнигамиToolStripMenuItem_Click);
+            // 
+            // управлениеПользователямиToolStripMenuItem
+            // 
+            this.управлениеПользователямиToolStripMenuItem.Name = "управлениеПользователямиToolStripMenuItem";
+            this.управлениеПользователямиToolStripMenuItem.Size = new System.Drawing.Size(234, 22);
+            this.управлениеПользователямиToolStripMenuItem.Text = "Управление пользователями";
+            this.управлениеПользователямиToolStripMenuItem.Click += new System.EventHandler(this.УправлениеПользователямиToolStripMenuItem_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Библиотека";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem импортэкспортToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem импортToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem экспортToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem окноToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem управлениеКнигамиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem управлениеПользователямиToolStripMenuItem;
+    }
+}
+
---
+++ Библиотека/Библиотека/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        Books booksForm;
+        private void УправлениеКнигамиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (booksForm == null)
+            {
+                booksForm = new Books();
+                booksForm.MdiParent = this;
+                booksForm.FormClosed += BooksForm_FormClosed;
+                booksForm.Show();
+            }
+            else
+                booksForm.Activate();
+        }
+
+        private void BooksForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            booksForm = null;
+            //throw new NotImplementedException();
+        }
+
+        Users usersForm;
+        private void УправлениеПользователямиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (usersForm == null)
+            {
+                usersForm = new Users();
+                usersForm.MdiParent = this;
+                usersForm.FormClosed += UsersForm_FormClosed;
+                usersForm.Show();
+            }
+        }
+
+        private void UsersForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            usersForm = null;
+            //throw new NotImplementedException();
+        }
+    }
+}
